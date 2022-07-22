c60c114bebd8778007fdf7ee7a42ee6dd595b474
rygailkim@gmail.com <rygailkim@gmail.com>
2022-07-22 23:02:47 +0800

    profile

--- quiz_app/quizapp/lib/screens/profile.dart
+++ quiz_app/quizapp/lib/screens/profile.dart
@@ -1,27 +1,72 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:quizapp/services/auth.dart';
+import 'package:quizapp/services/models.dart';
+import 'package:quizapp/shared/shared.dart';
 
-import '../shared/bottom_nav.dart';
-
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavBar(),
-      appBar: AppBar(
-        title: Text('Profile'),
-      ),
-      body: ElevatedButton(
-          child: Text('signout'),
-          onPressed: () async {
-            await AuthService().signOut();
-            Navigator.of(context)
-                .pushNamedAndRemoveUntil('/', (route) => false);
-          }),
-    );
+    var report = Provider.of<Report>(context);
+    // var user = AuthService().user;
+    var user = FirebaseAuth.instance.currentUser!;
+
+    if (user != null) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          title: Text(user.displayName ?? 'Guest'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                margin: const EdgeInsets.only(top: 50),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  // image: DecorationImage(
+                  // image: NetworkImage(user.photoURL ??
+                  //     'https://www.gravatar.com/avatar/placeholder'),
+                  // ),
+                ),
+              ),
+              Text(user.email ?? '',
+                  style: Theme.of(context).textTheme.headline6),
+              const Spacer(),
+              Text('${report.total}',
+                  style: Theme.of(context).textTheme.headline2),
+              Text('Quizzes Completed',
+                  style: Theme.of(context).textTheme.subtitle2),
+              const Spacer(),
+              ElevatedButton(
+                child: const Text('logout'),
+                onPressed: () async {
+                  await AuthService().signOut();
+                  if (mounted) {
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/', (route) => false);
+                  }
+                },
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return const Loader();
+    }
   }
 }
